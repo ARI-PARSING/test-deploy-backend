d1eed80d548affbbe876496436793081bad97ea1
HenryLima07 <00033721@uca.edu.sv>
2025-06-01 11:55:10 -0600

    :sparkles: add: handlers for parsing file to new file

--- src/service/convertFile.service.js
+++ src/service/convertFile.service.js
@@ -1,2 +1,9 @@
 import { fileExists, readFileJson, readFileTxt, readFileXml, removeFile } from '../utils/fileHandler.util.js';
 import { mapDataToListMap } from '../utils/mappingData.util.js';
+
+const fileConverter = async (filePath, fileType) => {
+};
+
+export {
+    fileConverter
+}
